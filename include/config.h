3663779628caf6393a9bb72cd9c9576e8eb0f4aa
Johannes Nilsson <nwj.nilsson@outlook.com>
2025-10-14 21:46:32 +0200

    Add config file

---
+++ include/config.h
@@ -0,0 +1,15 @@
+// IMPORTANT:
+//
+// Uncomment this if you want to use inches. This affects the default pierce and cut height values
+// that the program will fall back to if the gcode is missing these parameters.
+//
+// #define USE_INCH_DEFAULTS
+
+#if defined(USE_INCH_DEFAULTS)
+#  define SCALE(x) x / 25.4f
+#else
+#  define SCALE(x) x
+#endif
+
+// Default values (in mm), change as you see fit
+#define DEFAULT_MATERIAL_SIZE SCALE(500.f) // The work piece in CAM view (500x500)
